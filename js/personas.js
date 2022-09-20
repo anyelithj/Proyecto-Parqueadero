45ddacbf605f7f5dcb293743eb976c86e32cccb8
dtorof <dtoro.sistemas@gmail.com>
2022-09-20 17:50:40 -0500

    “personas”

--- js/personas.js
+++ js/personas.js
@@ -1,105 +1,62 @@
 var app = new Vue({
     el: '#app',
     data: {
-      genero:"",
-      sexo:[{tipo:"Masculino",vHora:25000},
-      {tipo:"Femenino",vHora:20000}],
-      total:0,
-      cedula:"",
-      empleado:"",
-      horas_Trabajadas:0,
-      empleados:[],
+      gender:"",
+      age:"",
+      sexo:[{tipo:"Masculino"},
+      {tipo:"Femenino"}],
+      id:"",
+      name:"",
+      phone:"",
+      people:[],
+      email:"",
       index:"",
-      error: false,
-      error2:false,
-      error3:false,
-      error4:false,
+
 
     },
     methods: {
-
-        getErroresCedula(){
-            if(this.cedula==''){
-              this.error=true;
-            }else{
-              this.error=false;
-            }
-          },
-          getErroresEmpleo(){
-            if(this.empleo == ''){
-              this.error2=true;
-            }else{
-              this.error2=false;
-            }
-          },
-
-          getErroresEmpleado(){
-            if(this.empleado=='' ){
-              this.error3=true;
-            }else{
-              this.error3=false;
-            }
-          },
-
-          getErroresHoras(){
-            if(this.horas_Trabajadas==''){
-              this.error4=true;
-            }else{
-              this.error4=false;
-            }
-          },
 
         getIndex(evt){
             
             this.index = evt.target.selectedIndex;
         },
+        clearInputs(){
+            this.age=""
+            this.id=""
+            this.name=""
+            this.email=""
+            this.phone=""
 
-        calcular(){
-            this.getErroresCedula();
-            this.getErroresEmpleado();
-            this.getErroresEmpleo();
-            this.getErroresHoras()
-            if(this.horas_Trabajadas<10){
-                alert("El valor de horas trabajadas debe ser mayor a 10")
-                this.horas_Trabajadas=""
-            }else if (this.horas_Trabajadas>100){
-                alert("El valor de horas trabajadas debe ser menor o igual a 100")
-                this.horas_Trabajadas=""
-            }else if (this.cedula.length==0 || this.empleado.length == 0 || this.empleo.length==0 || this.horas_Trabajadas.length==0){
+        },
+        deleteRegister(index){
+            this.people.splice(index,1)
+        },
+
+        calculate(){
+           
+      
+            this.people.push({
+                id: this.id,
+                age:this.age,
+                name: this.name,
+                phone:this.phone,
+                email:this.email,
+                gender:this.gender
                 
-            }
-            
-            else{
-                
-            this.total = this.sexo[this.index].vHora * this.horas_Trabajadas
-            this.empleados.push({
-                cedula: this.cedula,
-                empleado: this.empleado,
-                horas: this.horas_Trabajadas,
-                cargo: this.empleo,
-                valor: this.total
             })
-            this.limpiar()
-            }
+            this.clearInputs()
+            },
 
 
         },
-        eliminar(index){
-            this.empleados.splice(index,1)
-        },
+        
 
-        limpiar(){
-            this.empleo=""
-            this.cedula=""
-            this.empleado=""
-            this.horas_Trabajadas=""
-
-        }
+        
+        
 
         
         
         
        
       
-    }
-  })
+    })
