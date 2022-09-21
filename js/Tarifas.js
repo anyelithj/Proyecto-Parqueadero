99728af8616b8e80b461852b554acf0c03ca5fbb
Anyelith Jacobo <anyelithj@misena.edu.co>
2022-09-20 20:32:41 -0500

    add desing index commit

--- js/Tarifas.js
+++ js/Tarifas.js
@@ -1,7 +1,30 @@
 var app = new Vue({
     el: '#app',
     data: {
-       
+        time: "",
+        price: "",
+        optionVehicle: null,
+        rates: [],
+        error: false,
+        vehicle: ["carro", "moto", "bicicleta"],
     },
-    methods: {}
+    methods: {
+        addRates(){
+          this.rates.push({
+            timeRates : this.time,
+            priceRates: this.price,
+            vehicleRates: this.optionVehicle
+          })
+          this.clear()
+        },
+        deleteRates() {
+            this.rates.splice(this.index,1);
+        },
+        clear() {
+            this.time = "",
+            this.price = "",
+            this.vehicle = "",
+            this.optionVehicle= ""
+        }, 
+    }
 })
